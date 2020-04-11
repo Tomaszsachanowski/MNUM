43df563f314830141361d398cb9129056126a22a
Tomasz Sachanowski <tsachano@mion.elka.pw.edu.pl>
2020-04-11 21:44:22 +0200

    Used polyval method

--- Projekt_2/kod/aproksymacja.m
+++ Projekt_2/kod/aproksymacja.m
@@ -1,4 +1,4 @@
-function [a1,norma1,a2,norma2]=aproksymacja(X,Y,stopien)
+function [a1, norma1, a2, norma2]=aproksymacja(X,Y,stopien)
     rozmiar = size(X, 1); %macierz A
     A = zeros(rozmiar, stopien+1);
     %Wypelniamy macierz A potegami elementow x %
@@ -8,11 +8,13 @@ function [a1,norma1,a2,norma2]=aproksymacja(X,Y,stopien)
         end
     end
     %uk³ad równañ normalnych
-    a1 = (A'*A)\(A'*Y);
+    a1 = A'*A\A'*Y;
+    approximation1 = polyval(a1,X);
     %uk³ad równañ liniowych z macierz¹ R wynikaj¹c¹ z rozk³adu QR
-    [Q, R] = qrGS(A);
+    [Q, R] = Decompose_QR(A);
     a2 = R\(Q'*Y);
+    approximation2 = polyval(a2,X);
     % Wyliczanie normy euklidesowej
-    norma1 = norm(A*a1-Y);
-    norma2 = norm(A*a2-Y);
+    norma1 = norm(approximation1-Y);
+    norma2 = norm(approximation2-Y);
 end
