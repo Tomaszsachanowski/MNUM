5d4cc18fccac95f3320418cd23c4657c8a519f9d
Tomasz Sachanowski <tsachano@mion.elka.pw.edu.pl>
2020-04-11 21:45:40 +0200

    fix bug Array indexError

--- Projekt_2/kod/Decompose_QR.m
+++ Projekt_2/kod/Decompose_QR.m
@@ -1,18 +1,19 @@
 function [Q,R] = Decompose_QR(A)
-    n = size(A,1);
-    Q = zeros(n);
-    R = zeros(n);
-    d = zeros(1, n);
+    %rozk³ad QR (w¹ski) zmodyfikowanym algorytmem Grama-Schmidta dla macierzy m x n (m>=n) o rzêdzie n, rzeczywistej lub zespolonej
+    [m n] = size(A);
+    Q=zeros(m,n);
+    R=zeros(n,n);
+    d=zeros(1,n);
     %rozk³ad z kolumnami Q ortogonalnymi
-        for i=1:n
-            Q(:,i) = A(:,i);
-            R(i,i) = 1;
-            d(i) = Q(:,i)'*Q(:,i);
-            for j=i+1:n
-                R(i,j)=(Q(:,i)'*A(:,j))/d(i);
-                A(:,j)=A(:,j)-R(i,j)*Q(:,i);
-            end
+    for i=1:n
+        Q(:,i) = A(:,i);
+        R(i,i) = 1;
+        d(i) = Q(:,i)'*Q(:,i);
+        for j=i+1:n
+            R(i,j)=(Q(:,i)'*A(:,j))/d(i);
+            A(:,j)=A(:,j)-R(i,j)*Q(:,i);
         end
+    end
     %normowanie rozk³adu (kolumny Q ortonormalne)
     for i=1:n
         dd=norm(Q(:,i));
@@ -20,4 +21,3 @@ function [Q,R] = Decompose_QR(A)
         R(i,i:n)=R(i,i:n)*dd;
     end
 end
-
