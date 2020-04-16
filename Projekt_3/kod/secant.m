26b205511543fab56166fbf79f6ab91f5ab03d24
Tomasz Sachanowski <tsachano@mion.elka.pw.edu.pl>
2020-04-16 22:47:12 +0200

    fix isolation condition

--- Projekt_3/kod/secant.m
+++ Projekt_3/kod/secant.m
@@ -4,17 +4,18 @@ function [c, iter] = secant(a, b, eps, iter_max)
     value_c = func(c); % wartosc dla srodka (rozwiazanie)
     tmp_a = a;
     tmp_b= b;
+    value_a = func(tmp_a); % wartosci na koncach przedzialu
+    value_b = func(tmp_b);
+    %sprawdzamy obszar izolacji
+    if(value_a*value_b>0)
+        error('Brak obszaru izolacji');
+    end
     while((abs(value_c) > eps)&& iter <= iter_max)
         % koniec gdy osiagniemy dobra dokladnosc albo limit iteracji
         value_a = func(tmp_a); % wartosci na koncach przedzialu
         value_b = func(tmp_b);
-        %sprawdzamy obszar izolacji
-        if(value_a*value_b>0)
-            error('Brak obszaru izolacji');
-        end
         c = (tmp_a*value_b - tmp_b*value_a)/(value_b - value_a); % nowa wartosc
         % dla metody siecznych
-
         value_c = func(c); % wartosc dla siecnych (rozwiazanie)
         tmp_a = b;
         tmp_b = c;
