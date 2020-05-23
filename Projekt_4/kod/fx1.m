5d141eec6b87c45993012f3b76519eb53770da5f
Tomasz Sachanowski <tsachano@mion.elka.pw.edu.pl>
2020-05-23 21:07:25 +0200

    Rename

--- Projekt_4/kod/fx1.m
+++
@@ -1,7 +0,0 @@
-function [y1,y2] = fx1(x1,x2)
-%FX1 Summary of this function goes here
-%   Detailed explanation goes here
-y1 = x2 + x1*(0.9 - x1^2 - x2^2);
-y2 = -x1 + x2*(0.9 - x1^2 - x2^2);
-end
-
